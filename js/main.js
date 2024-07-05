62fea584089b0d5ce4d3bed79bc617b46f7df5df
chelangatbrigit <brigitchelangat@gmail.com>
2024-07-05 17:04:49 +0300

    Login design

--- js/main.js
+++ js/main.js
@@ -12,15 +12,25 @@ fetch("data/courses.json")
      <div class="col-12 col-lg-4 col-md-6 mb-4">
       <div class="card">
        <div class="card-body">
-        <img src="images/gdesign.png" alt="Logo" class="img-fluid" width="40" height="40" style="border-radius: 50px" />
-        <i class="bi bi-three-dots" style="margin-left: 245px; font-size: 10px; color: gray"></i>
+       <span style="
+            font-weight: bolder;
+            font-size: 15px;
+            display:inline;
+          ">
+          ${post.course_code}
+       
+        </span>
+        
+          <i class="bi bi-three-dots" style="margin-left: 240px; font-size: 10px; color: gray "></i>
         <p style="
             font-weight: 500;
             font-size: 15px;
-            margin-bottom: 4px;
+            margin-bottom: 7px;
           ">
           ${post.title}
+       
         </p>
+        
         <img src="images/brigitimage.jpeg" alt="Logo" class="img-fluid" width="25" height="25" style="border-radius: 50px" />
         <span style="font-weight: 400; font-size: 11px; color: gray">${post.instructor}</span>
         <p style="margin-bottom: 5px">
@@ -48,12 +58,15 @@ fetch("data/courses.json")
             "></i>
           <span style="font-weight: 400; font-size: 10px; color: gray">${post.hours} Hours</span>
         </p>
-        <button class="btn btn-secondary" onclick="enroll(${post.id})">Enroll</button>
+        <div class="mt-3"> 
+          <button style="font-size: 10px padding:10px" class="btn btn-secondary enroll-button" onclick="enroll(${post.id})">Enroll</button>
+        </div>
+        
        </div>
       </div>
      </div>
     `;
-
+    
 
     });
   })
@@ -61,6 +74,13 @@ fetch("data/courses.json")
 }
 main();
 
+ // Add click event listener to toggle button visibility
+ document.querySelector('.card').addEventListener('click', function() {
+    const enrollButton = this.querySelector('.enroll-button');
+    enrollButton.classList.toggle('visible');
+  });
+
+  
 function enroll(courseId) {
     $('#checkoutModal').modal('show');
     $('#proceedCheckout').off('click').on('click', function() {
